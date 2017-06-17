ee9a63f515aefd3b12d22b8e47d9a2bf7f9e6a34
Michael Peyper <michael.peyper@ioof.com.au>
2017-06-18 00:12:19 +1000

    Pass props when resolving array of actions

--- src/handleAction.js
+++ src/handleAction.js
@@ -3,7 +3,7 @@ const handleAction = (action, dispatch, props) => {
   if (typeof action === 'function') {
       dispatch(action(props))
   } else if (Array.isArray(action)) {
-      action.forEach(a => handleAction(a, dispatch))
+      action.forEach(a => handleAction(a, dispatch, props))
   } else {
       dispatch(action)
   }
